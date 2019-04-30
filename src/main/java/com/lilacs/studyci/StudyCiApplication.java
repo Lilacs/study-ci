9147f71abc43e69810ce4d7603f561901e5a0a18
zhangpeng <zp1990@me.com>
2019-05-01 00:26:37 +0800

    init

---
+++ src/main/java/com/lilacs/studyci/StudyCiApplication.java
@@ -0,0 +1,13 @@
+package com.lilacs.studyci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyCiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyCiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lilacs/studyci/StudyCiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lilacs.studyci;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StudyCiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
